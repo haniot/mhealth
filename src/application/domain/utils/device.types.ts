0e4309409db2cc516267e358c320450ef33b9c09
Lucas Rocha <lucas_rocha2@hotmail.com>
2019-08-14 16:48:11 -0300

    Update device.types.ts

--- src/application/domain/utils/device.types.ts
+++ src/application/domain/utils/device.types.ts
@@ -4,5 +4,6 @@ export enum DeviceTypes {
     BODY_COMPOSITION = 'body_composition',
     BLOOD_PRESSURE = 'blood_pressure',
     SMARTWATCH = 'smartwatch',
+    HEART_RATE = 'heart_rate',
     SMARTBAND = 'smartband'
 }
